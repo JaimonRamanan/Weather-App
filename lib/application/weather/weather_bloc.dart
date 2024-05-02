32c560bdf0f73b6b6337f0379592b08ad95c16e7
Jaimon Ramanan <jaimonramanan@Jaimons-MacBook-Air-10.local>
2024-05-02 19:49:33 +0530

    feat: business logic: get data from api

--- lib/application/weather/weather_bloc.dart
+++ lib/application/weather/weather_bloc.dart
@@ -1,8 +1,8 @@
+import 'package:injectable/injectable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:injectable/injectable.dart';
-import 'package:weather_pulse/core/api_response/api_response.dart';
 import 'package:weather_pulse/domain/weather/i_weather_repo.dart';
+import 'package:weather_pulse/core/api_response/api_response.dart';
 
 part 'weather_state.dart';
 part 'weather_event.dart';
@@ -13,8 +13,24 @@ class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
   final IWeatherRepo repo;
   WeatherBloc(this.repo) : super(WeatherState.initial()) {
     on<_GetWeather>(
-      (event, emit) {
-        emit(state.copyWith.weatherRes(loading: true));
+      (event, emit) async {
+        emit(state.copyWith.weatherRes(loading: true, error: null));
+        try {
+          final response = await repo.getCurrentWeatherForACity(
+            cityName: event.city,
+          );
+
+          response.fold(
+            (faiure) {
+              emit(state.copyWith.weatherRes(loading: true, error: faiure));
+            },
+            (res) {
+              emit(state.copyWith.weatherRes(loading: true, data: res));
+            },
+          );
+        } catch (e) {
+          emit(state.copyWith.weatherRes(loading: false));
+        }
       },
     );
   }
--- lib/domain/weather/i_weather_repo.dart
+++ lib/domain/weather/i_weather_repo.dart
@@ -1,8 +1,9 @@
 import 'package:dartz/dartz.dart';
 import 'package:weather_pulse/core/failure/failure.dart';
+import 'package:weather_pulse/domain/weather/models/weather_model/weather_model.dart';
 
 abstract class IWeatherRepo {
-  Future<Either<Failure, dynamic>> getCurrentWeatherForACity({
+  Future<Either<Failure, WeatherModel>> getCurrentWeatherForACity({
     required String cityName,
   });
 }
