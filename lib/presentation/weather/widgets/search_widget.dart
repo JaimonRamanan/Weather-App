97873c8ad73339444b30c6235ece860d49c8e1ee
Jaimon Ramanan <jaimonramanan@Jaimons-MacBook-Air-10.local>
2024-05-02 22:06:18 +0530

    fix: text field validation

--- lib/presentation/weather/widgets/search_widget.dart
+++ lib/presentation/weather/widgets/search_widget.dart
@@ -7,37 +7,54 @@ import '../../../core/route/route_names.dart';
 import '../../widgets/common_text_field.dart';
 import '../../../application/weather/weather_bloc.dart';
 
-class SearchWidget extends StatelessWidget {
+class SearchWidget extends StatefulWidget {
   const SearchWidget({super.key});
 
   @override
+  State<SearchWidget> createState() => _SearchWidgetState();
+}
+
+class _SearchWidgetState extends State<SearchWidget> {
+  final _formKey = GlobalKey<FormState>();
+  @override
   Widget build(BuildContext context) {
     final bloc = context.read<WeatherBloc>();
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        CommonTextField(
-          hintText: "Search for city",
-          textEditingController: bloc.cityNameCtr,
-        ),
-        SizedBox(height: 20.h),
-        BlocConsumer<WeatherBloc, WeatherState>(
-          listener: (context, state) {
-            if (state.weatherRes.data != null) {
-              Navigator.pushNamed(context, RouteNames.detailScreen);
-            }
-          },
-          builder: (context, state) {
-            return CommonButton(
-              btnName: "Get weather",
-              loading: state.weatherRes.loading,
-              onTap: () {
-                bloc.add(const WeatherEvent.getWeather());
-              },
-            );
-          },
-        )
-      ],
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CommonTextField(
+            hintText: "Search for city",
+            textEditingController: bloc.cityNameCtr,
+            validator: (city) {
+              if (city?.isEmpty ?? true) {
+                return "Enter a city name";
+              }
+              return null;
+            },
+          ),
+          SizedBox(height: 20.h),
+          BlocConsumer<WeatherBloc, WeatherState>(
+            listener: (context, state) {
+              if (state.weatherRes.data != null) {
+                Navigator.pushNamed(context, RouteNames.detailScreen);
+              }
+            },
+            builder: (context, state) {
+              return CommonButton(
+                btnName: "Get weather",
+                loading: state.weatherRes.loading,
+                onTap: () {
+                  if (_formKey.currentState?.validate() ?? false) {
+                    bloc.add(const WeatherEvent.getWeather());
+                  }
+                },
+              );
+            },
+          )
+        ],
+      ),
     );
   }
 }
